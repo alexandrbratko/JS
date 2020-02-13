9d87d3d2df24db3015aca5d1a81c7b994ab5be18
Alexandr Bratko <sashabratko89@gmail.com>
2020-02-13 13:03:06 +0200

    Lessons first

---
+++ lessons#1/script.js
@@ -0,0 +1,21 @@
+let money = +prompt("Ваш бюджет на месяц?", "");
+let time = +prompt("Введите дату в формате YYYY-MM-DD", "");
+
+const appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  saving: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let a2 = prompt("Во сколько єто обойдется", "");
+let a3 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let a4 = prompt("Во сколько єто обойдется", "");
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+alert(appData / 30);
