46bb700cdee0e340463cfd887fd3bb1a68409648
AlexandrBratko <sashabratko89@gmail.com>
2020-04-09 22:07:19 +0300

    add modules slider

--- precticle_classic_style_v2/src/js/main.js
+++ precticle_classic_style_v2/src/js/main.js
@@ -1,10 +1,12 @@
-'use strick';
 import modals from './modules/modals';
+import sliders from './modules/sliders';
 
 
 window.document.addEventListener('DOMContentLoaded', function () {
+	'use strict';
 
 	modals();
 
-
+	sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+	sliders('.main-slider-item', 'vertical');
 });
